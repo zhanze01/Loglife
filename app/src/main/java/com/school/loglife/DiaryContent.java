5b2fb96244ae8e19987a23f584341670a04c274c
sebpl <seb.plaschy@gmail.com>
2024-03-20 20:46:07 +0100

    up to date funktioniert

--- app/src/main/java/com/school/loglife/DiaryContent.java
+++ app/src/main/java/com/school/loglife/DiaryContent.java
@@ -1,10 +1,19 @@
 package com.school.loglife;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationChannelCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
